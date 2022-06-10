92ff85963a8fca0d1e51fa8e32cbbaf004c455db
gargithakare <86371591+gargithakare@users.noreply.github.com>
2022-06-10 21:22:49 +0530

    Add files via upload

---
+++ Dice Challenge/index.js
@@ -0,0 +1,24 @@
+
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+
+var image1 = document.querySelectorAll("img") [0].setAttribute("src", randomImageSource);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageSource2 = "images/dice" +  randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img") [1].setAttribute("src", randomImageSource2);
+
+
+
+if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🚩Player 2 wins!"
+}
+else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 wins!"
+}
+else {
+  document.querySelector("h1").innerHTML = "🚩Draw!"
+}
